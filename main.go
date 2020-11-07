c61ba0e5b41825ec93a160167294dbb2794b5fd5
Tobias Renwick <renwick@ualberta.ca>
2020-11-07 12:29:04 -0700

    init

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "log"
+    // "io/ioutil"
+    "net/http"
+    "html/template"
+)
+
+
+type StoryPage struct {
+    Title string
+    Story [][]string
+    Spans [][]int
+}
+
+type Page struct {
+
+}
+
+// func loadPage(title string) (*Page, error) {
+//     filename := title + ".txt"
+//     body, err := ioutil.ReadFile(filename)
+//     if err != nil {
+//         return nil, err
+//     }
+//     return &Page{Title: title, Body: body}, nil
+// }
+
+func introHandler(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("pages/intro.html")
+    p := Page {}
+    t.Execute(w,p)
+}
+
+func storyHandler(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("pages/story.html")
+    s := StoryPage {"This old man", [][]string{{"l1","one"}, {"l2","two"}, {"l3","three"}, {"four"}, {"five"}, {"six"}}, [][]int{{1,2}}}
+    t.Execute(w,s)
+}
+func main() {
+    http.HandleFunc("/", introHandler)
+    http.HandleFunc("/story/", storyHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+

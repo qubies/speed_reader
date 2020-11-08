71ce5f8352d3c69c59d1c4e60684e0f9f05cd6b5
Tobias Renwick <renwick@ualberta.ca>
2020-11-08 11:23:39 -0700

    swicthcto gin

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
     "log"
     "io/ioutil"
-    "net/http"
     "html/template"
     "os"
     "github.com/joho/godotenv"
     "encoding/json"
+    "github.com/gin-gonic/gin"
+    "fmt"
 )
 
 // globals 
@@ -17,28 +18,20 @@ var templates *template.Template
 
 type StoryPage struct {
     Title string
+    User string
     CommonWords []string
     Story [][]string
     Spans [][]int
 }
 
-type Page struct {
-}
 
-func (SP *StoryPage) handle_request(w http.ResponseWriter, r *http.Request) {
-    err := templates.ExecuteTemplate(w, "story.html", SP)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+func (SP *StoryPage) handle_request(c *gin.Context) {
+    c.HTML(200, "story.html", SP)
 }
 
 
-func introHandler(w http.ResponseWriter, r *http.Request) {
-    p := Page {}
-    err := templates.ExecuteTemplate(w, "intro.html", p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+func introHandler(c *gin.Context) {
+    c.HTML(200, "intro.html", nil)
 }
 
 func get_common_words() []string {
@@ -61,20 +54,30 @@ func init() {
         log.Print("No .env file found")
     }
     templates = template.Must(template.ParseFiles("pages/quiz.html", "pages/intro.html", "pages/story.html"))
+    fmt.Printf("%+v\n", templates)
 }
 
 func main() {
-    s := StoryPage {"This old man", common_words, [][]string{{"l1","one,"}, {"l2","two."}, {"l3","three"}, {"four"}, {"five"}, {"six"}}, [][]int{{1,2}}}
+    s := StoryPage {"This old man", "user", common_words, [][]string{{"l1","one,"}, {"l2","two."}, {"l3","three"}, {"four"}, {"five"}, {"six"}}, [][]int{{1,2}}}
     PORT := os.Getenv("PORT")
     if PORT == "" {
         PORT = "80"
     }
+    app := gin.Default()
+    app.SetHTMLTemplate(templates)
 
-    http.HandleFunc("/", introHandler)
-    http.HandleFunc("/story/", s.handle_request)
-    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-    http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
+    //routing
+    app.Static("/css","./css")
+    app.Static("/scripts","./scripts")
+    app.GET("/story", s.handle_request)
+    app.GET("/", introHandler)
+    app.Run(":"+PORT)
 
-    log.Fatal(http.ListenAndServe(":"+PORT, nil))
+    // http.HandleFunc("/", introHandler)
+    // http.HandleFunc("/story/", s.handle_request)
+    // http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+    // http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("scripts"))))
+
+    // log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
 

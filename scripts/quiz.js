a280b57bc94653310deacd14fd1cd60b16610227
Tobias Renwick <renwick@ualberta.ca>
2020-11-10 08:53:46 -0700

    remove prints

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -132,10 +132,7 @@ var Question = function(question_string, correct_choice, wrong_choices) {
 
     // Random assign the correct choice an index
     this.correct_choice_index = getRandom(0, wrong_choices.length + 1);
-    console.log(`Correct Choice index: ${this.correct_choice_index}`);
-    console.log(`Wrong Choce len: ${wrong_choices.length}`);
 
-  
   // Fill in this.choices with the choices
   var number_of_choices = wrong_choices.length + 1;
   for (var i = 0; i < number_of_choices; i++) {
@@ -145,7 +142,6 @@ var Question = function(question_string, correct_choice, wrong_choices) {
       // Randomly pick a wrong choice to put in this index
       var wrong_choice_index = Math.floor(Math.random(0, wrong_choices.length));
       this.choices[i] = wrong_choices[wrong_choice_index];
-      
       // Remove the wrong choice from the wrong choice array so that we don't pick it again
       wrong_choices.splice(wrong_choice_index, 1);
     }
